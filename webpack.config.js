8840e12d39b5cd361abf69306a1f7e662d29dd64
Brandon Chung <jbx.chung@gmail.com>
2024-04-04 00:11:53 -0400

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const rawParams = process.argv.slice(2);
+const cliParams = {};
+for (let i = 0; i < rawParams.length - 1; i += 2) {
+  const argName = rawParams[i].replace(/-/g, '');
+  cliParams[argName] = rawParams[i + 1];
+}
+
 module.exports = {
   entry: './src/index.tsx',
   output: { path: path.join(__dirname, 'dist'), filename: 'index.bundle.js' },
-  mode: process.env.NODE_ENV || 'development',
+  mode: cliParams.mode || 'development',
   resolve: {
       extensions: ['.tsx', '.ts', '.js'],
   },
@@ -33,6 +40,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
+      favicon: path.join(__dirname, 'src', 'favicon.ico'),
     }),
   ],
   devServer: {
