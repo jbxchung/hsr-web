e2c5c9e7c3dc5d9e0a620fb8eb3ef5f2f8f11c3d
Brandon Chung <jbx.chung@gmail.com>
2024-04-22 12:38:05 -0400

    refactor Friends page

--- src/pages/Friends/FriendRow.tsx
+++ src/pages/Friends/FriendRow.tsx
@@ -1,73 +1,20 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
-import { User, UserRole } from '../../types/User';
-import { useAuth } from '../../hooks/useAuth';
-import EditIcon from '../../components/Icons/Edit';
-import DeleteIcon from '../../components/Icons/Delete';
-import SaveIcon from '../../components/Icons/Save';
-import { Friendship, FriendshipStatus } from '../../types/Friendship';
-import { useFriendAccept, useFriendCancel, useFriendReject } from '../../hooks/useFriends';
-import CancelIcon from '../../components/Icons/Cancel';
-import CheckCircle from '../../components/Icons/CheckCircle';
+import { FC } from 'react';
+
+import { FriendAction } from './Friends';
 
 interface FriendRowProps {
-  currentUser: User | null;
-  friendship: Friendship;
+  displayName: string;
+  actions: Array<FriendActionMap>;
+  statusText: string;
 }
 
-// TODO - just display stuff, leave all this login in Friends.tsx
-const FriendRow: FC<FriendRowProps> = ({ currentUser, friendship }) => {
-  const [displayName, setDisplayName] = useState<string>('');
-  const [statusText, setStatusText] = useState<string>('');
-  const [canCancel, setCanCancel] = useState<boolean>(false);
-  const [canAccept, setCanAccept] = useState<boolean>(false);
-  const [canReject, setCanReject] = useState<boolean>(false);
-  const [canRemove, setCanRemove] = useState<boolean>(false);
+interface FriendActionMap {
+  action: FriendAction;
+  call: Function;
+  icon: FC;
+}
 
-  const { friendship: cancelledFriend, invoke: cancelFriendRequest } = useFriendCancel();
-  const { friendship: acceptedFriend, invoke: acceptFriendRequest } = useFriendAccept();
-  const { friendship: rejectedFriend, invoke: rejectFriendRequest } = useFriendReject();
-
-  useEffect(() => {
-    if (cancelledFriend) {
-      setCanCancel(false);
-      setStatusText('Cancelled');
-    }
-  }, [cancelledFriend]);
-
-  useEffect(() => {
-    if (acceptedFriend) {
-      setCanReject(false);
-      setCanRemove(true);
-      setCanAccept(false);
-      setStatusText('Friends');
-    }
-  }, [acceptedFriend]);
-
-  useEffect(() => {
-    if (currentUser && friendship) {
-      if (friendship.status === FriendshipStatus.ACCEPTED) {
-        // already friends, can remove
-        setCanRemove(true);
-        setStatusText('Friends');
-        setDisplayName((friendship.sender === currentUser.username) ? friendship.receiver: friendship.sender);
-      } else if (friendship.sender === currentUser.username) {
-        // we sent this request, display the receiver's name
-        setDisplayName(friendship.receiver);
-        // pending request, can cancel
-        setCanCancel(true);
-        setStatusText('Sent Request');
-      } else {
-        // we received this request, display the sender's name
-        setDisplayName(friendship.sender);
-        // pending request, can accept or reject
-        setCanAccept(true);
-        setCanReject(true);
-        setStatusText('Received Request');
-      }
-    }
-  }, [currentUser, friendship]);
-  
-
+const FriendRow: FC<FriendRowProps> = ({ displayName, statusText, actions }) => {
   return (
     <tr className={`friend-row`}>
       <td>
@@ -78,42 +25,15 @@ const FriendRow: FC<FriendRowProps> = ({ currentUser, friendship }) => {
       </td>
       <td>
         <div className="action-icons">
-        {canCancel &&
+        {actions.map((a) => (
           <span
-            className="action-icon cancel"
-            title="Cancel"
-            onClick={() => cancelFriendRequest({ user: displayName })}
+            className={`action-icon ${a.action.toLowerCase()}`}
+            title={a.action}
+            onClick={() => a.call(displayName)}
           >
-            <CancelIcon />
+            <a.icon />
           </span>
-        }
-        {canRemove &&
-          <span
-            className="action-icon remove"
-            title="Remove"
-            onClick={() => alert('todo: Allow removing friends via UI')}
-          >
-            <DeleteIcon />
-          </span>
-        }
-        {canAccept &&
-          <span
-            className="action-icon accept"
-            title="Accept"
-            onClick={() => acceptFriendRequest({ user: displayName })}
-          >
-            <CheckCircle />
-          </span>
-        }
-        {canReject &&
-          <span
-            className="action-icon reject"
-            title="Reject"
-            onClick={() => rejectFriendRequest({ user: displayName })}
-          >
-            <CancelIcon />
-          </span>
-        }
+        ))}
         </div>
       </td>
     </tr>
--- src/pages/Friends/Friends.tsx
+++ src/pages/Friends/Friends.tsx
@@ -3,25 +3,38 @@ import { FC, useCallback, useEffect, useState } from 'react';
 import Loader from '../../components/Loader/Loader';
 
 import { useAuth } from '../../hooks/useAuth';
-import { useFriendRequest, useFriends } from '../../hooks/useFriends';
+import * as friendsApi from '../../hooks/useFriends';
 import { Friendship, FriendshipStatus } from '../../types/Friendship';
 
 import './Friends.scss';
 import FriendRow from './FriendRow';
 import PlusCircle from '../../components/Icons/PlusCircle';
+import CancelIcon from '../../components/Icons/Cancel';
+import CheckCircle from '../../components/Icons/CheckCircle';
+import DeleteIcon from '../../components/Icons/Delete';
+
+export enum FriendAction {
+  ACCEPT = 'Accept',
+  DELETE = 'Remove',
+  CANCEL = 'Cancel',
+  REJECT = 'Reject',
+}
 
 const Friends: FC = () => {
   const { user } = useAuth();
-  const { friends, isLoading } = useFriends();
-  const { friendship: newFriendRequest, invoke: sendFriendRequest } = useFriendRequest();
+  const { friends, isLoading } = friendsApi.useFriends();
+  const { friendship: newFriendRequest, invoke: sendFriendRequest } = friendsApi.useFriendRequest();
+  const { friendship: acceptedFriend, invoke: acceptFriendRequest } = friendsApi.useFriendAccept();
+  const { friendship: cancelledFriend, invoke: cancelFriendRequest } = friendsApi.useFriendCancel();
+  const { friendship: rejectedFriend, invoke: rejectFriendRequest } = friendsApi.useFriendReject();
 
   const [sentRequests, setSentRequests] = useState<Array<Friendship>>([]);
   const [receivedRequests, setReceivedRequests] = useState<Array<Friendship>>([]);
   const [friendList, setFriendList] = useState<Array<Friendship>>([]);
 
   const [addFriendName, setAddFriendName] = useState<string>('');
-
-  // when a new friend request has been sent, update our local state to display it
+  
+  // add new friend request to display
   useEffect(() => {
     if (newFriendRequest) {
       const sent = [...sentRequests];
@@ -29,6 +42,25 @@ const Friends: FC = () => {
     }
   }, [newFriendRequest]);
 
+  // remove cancelled request
+  useEffect(() => {
+    if (cancelledFriend) {
+      const sent = [...sentRequests];
+      sent.splice(sent.findIndex(f => f.receiver === cancelledFriend.receiver), 1);
+      setSentRequests(sent);
+    }
+  }, [cancelledFriend]);
+
+  // remove rejected request
+  useEffect(() => {
+    if (rejectedFriend) {
+      const received = [...receivedRequests];
+      received.splice(received.findIndex(f => f.sender === rejectedFriend.sender), 1);
+      setReceivedRequests(received);
+    }
+  }, [rejectedFriend]);
+
+  // when we get the friends list, split it by sent/received/confirmed to display in that order
   useEffect(() => {
     const sent: Array<Friendship> = [];
     const received: Array<Friendship> = [];
@@ -74,9 +106,30 @@ const Friends: FC = () => {
         )}
         <table className="friends-table">
           <tbody>
-            {sentRequests.map(f => <FriendRow key={JSON.stringify(f)} currentUser={user} friendship={f} />)}
-            {receivedRequests.map(f => <FriendRow key={JSON.stringify(f)} currentUser={user} friendship={f} />)}
-            {friendList.map(f => <FriendRow key={JSON.stringify(f)} currentUser={user} friendship={f} />)}
+            {sentRequests.map(f => (
+              <FriendRow
+                displayName={f.receiver}
+                statusText="Friend request sent"
+                actions={[{ action: FriendAction.CANCEL, call: cancelFriendRequest, icon: CancelIcon } ]}
+              />
+            ))}
+            {receivedRequests.map(f => (
+              <FriendRow
+                displayName={f.sender}
+                statusText="Friend request received"
+                actions={[
+                  { action: FriendAction.ACCEPT, call: acceptFriendRequest, icon: CheckCircle },
+                  { action: FriendAction.REJECT, call: rejectFriendRequest, icon: DeleteIcon },
+                ]}
+              />
+            ))}
+            {friendList.map(f => (
+              <FriendRow
+                displayName={(f.sender === user?.username) ? f.receiver: f.sender}
+                statusText="Friend request accepted"
+                actions={[{ action: FriendAction.DELETE, call: cancelFriendRequest, icon: CancelIcon }]}
+              />
+            ))}
           </tbody>
         </table>
       </div>
