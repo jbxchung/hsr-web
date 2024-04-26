14de0d82d8d31bd2e464f21a67b5e8d2eed42728
Brandon Chung <jbx.chung@gmail.com>
2024-04-25 23:14:03 -0400

    show character details modal based on url path

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useGameEntities } from '../../hooks/useGameEntities';
 import { GameEntityType } from '../../types/GameEntity';
 import CardList from '../../components/Card/CardList';
@@ -6,25 +6,45 @@ import Loader from '../../components/Loader/Loader';
 
 import './Characters.scss';
 import NewCharacterForm from './NewCharacterForm';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Character } from '../../types/Character';
+import CharacterDetails from './CharacterDetails';
 
 const Characters: FC = () => {
   const { response: characters, isLoading, error, invoke: refreshCharacterList } = useGameEntities(GameEntityType.CHARACTER);
-
   const [showNewCharacterForm, setShowNewCharacterForm] = useState<boolean>(false);
+  
+  // decide if we should show a modal of a specific character based on url path
+  const location = useLocation();
+  const navigate = useNavigate();
+  let characterDetail = null;
+  const splitPath = location.pathname.split('/').filter(pathSegment => !!pathSegment);
+  if (splitPath[1]) {
+    characterDetail = characters?.find(c => c.id === splitPath[1]) as Character;
+  }
 
   return (
     <div className="characters">
       <div className="filter-bar">
         todo: implement filter and search
       </div>
-      {showNewCharacterForm && <NewCharacterForm onCharacterCreated={refreshCharacterList}
-        closeModal={() => setShowNewCharacterForm(false)} />
-      }
       {isLoading && (
         <div className="loading-overlay">
           <Loader />
         </div>
       )}
+      {showNewCharacterForm &&
+        <NewCharacterForm
+          onCharacterCreated={refreshCharacterList}
+          closeModal={() => setShowNewCharacterForm(false)}
+        />
+      }
+      {characterDetail &&
+        <CharacterDetails
+          character={characterDetail}
+          closeModal={() => navigate('/characters')}
+        />
+      }
       {characters && <CardList entities={characters} entityType={GameEntityType.CHARACTER} createNew={() => setShowNewCharacterForm(true)} />}
     </div>
   );
